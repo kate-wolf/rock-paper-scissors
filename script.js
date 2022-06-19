5b2430404724c35148d0516ba1fdc1d4ff8ec142
Kate Wolf <whoiskateidkher@gmail.com>
2022-06-19 13:40:05 -0400

    set up rps ui

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 function computerPlay() {
-  // create number with value of random number
   const number = Math.random();
-  // if number <= .333 return rock
   if (number <= .333) return 'rock';
-  // if number <= .667 return paper
   if (number <= .667) return 'paper';
-  // if numbeer <= 1 return scissors
   if (number <= 1) return 'scissors';
 }
 
@@ -17,9 +13,7 @@ function playRound(playerSelection, computerSelection) {
   if (!(playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors')) {
     return 'Invalid input! Enter only rock, paper, or scissors.';
   }
-  // tell player what they picked
   console.log('You chose ' + playerSelection);
-  // tell player what computer picked
   console.log('Computer chose ' + computerSelection);
   // if tie tell player
   if (playerSelection === computerSelection) return 'It\'s a tie! Try again';
@@ -33,34 +27,39 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-
-// console.log(playRound(playerSelection, computerSelection));
-
 function game() {
   let playerWinCount = 0;
   let computerWinCount = 0;
-  for (let i = 0; i < 5; i++) {
-    // your code here!
-    const playerSelection = prompt('Choose rock, paper, or scissors: ');
-    const computerSelection = computerPlay();
-    let round = playRound(playerSelection, computerSelection);
-    // if (round === 'Invalid input! Enter only rock, paper, or scissors.' || 'Enter a value!'){
-    //   i--;
-    // } else
-    // if (round === 'It\'s a tie! Try again') {
-    //   i--;
-    // } else
-    if (round === 'You win!') {
-      playerWinCount++;
-    } else
-    if (round === 'You lose!') {
-      computerWinCount++;
-    }
-    console.log(round);
- }
+//   for (let i = 0; i < 5; i++) {
+//     const playerSelection = prompt('Choose rock, paper, or scissors: ');
+//     const computerSelection = computerPlay();
+//     let round = playRound(playerSelection, computerSelection);
+//     if (round === 'You win!') {
+//       playerWinCount++;
+//     } else
+//     if (round === 'You lose!') {
+//       computerWinCount++;
+//     }
+//     console.log(round);
+//  }
  if (playerWinCount > computerWinCount) return 'You win the game!';
  if (playerWinCount < computerWinCount) return 'You lose the game!';
  if (playerWinCount == computerWinCount) return 'The game is a tie!';
 }
 
-console.log(game());
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+
+rock.addEventListener("click", function() {
+  playRound(this.id, computerPlay())
+});
+paper.addEventListener("click", function() {
+  playRound(this.id, computerPlay())
+});
+scissors.addEventListener("click", function() {
+  playRound(this.id, computerPlay())
+});
+
+
+// console.log(game());
