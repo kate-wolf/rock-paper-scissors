376b077f4c854ed832791baa177190bf4f8ed10c
Kate Wolf <whoiskateidkher@gmail.com>
2022-06-20 13:04:08 -0400

    created ui version of rock paper scissors

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function computerPlay() {
   if (number <= .333) return 'rock';
   if (number <= .667) return 'paper';
   if (number <= 1) return 'scissors';
+  
 }
 
 
@@ -19,17 +20,25 @@ function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) return 'It\'s a tie! Try again';
   // when player wins return 'You won!'
   if (playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'paper' && computerSelection === 'rock' || playerSelection === 'scissors' && computerSelection === 'paper') {
-    return 'You win!';
+    playerWinCount++;
   }
   // when player loses return 'You lose!'
   if (playerSelection === 'scissors' && computerSelection === 'rock' || playerSelection === 'paper' && computerSelection === 'scissors' || playerSelection === 'rock' && computerSelection === 'paper') {
-    return 'You lose!';
+    computerWinCount++;
+  }
+  result.innerText = `${playerWinCount} - ${computerWinCount}`;
+  document.getElementById("results").appendChild(result);
+  if(computerWinCount >= 5) {
+    winner.innerText = "You lost!";
+    document.getElementById("results").appendChild(winner);
+  }
+  if(playerWinCount >= 5) {
+    winner.innerText = "You won!";
+    document.getElementById("results").appendChild(winner);
   }
 }
 
-function game() {
-  let playerWinCount = 0;
-  let computerWinCount = 0;
+
 //   for (let i = 0; i < 5; i++) {
 //     const playerSelection = prompt('Choose rock, paper, or scissors: ');
 //     const computerSelection = computerPlay();
@@ -42,14 +51,22 @@ function game() {
 //     }
 //     console.log(round);
 //  }
- if (playerWinCount > computerWinCount) return 'You win the game!';
- if (playerWinCount < computerWinCount) return 'You lose the game!';
- if (playerWinCount == computerWinCount) return 'The game is a tie!';
-}
+
+
+const result = document.createElement("p");
+const winner = document.createElement("p");
+result.innerText = "0 - 0";
+document.getElementById("results").appendChild(result);
+
 
 const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
+
+
+let playerWinCount = 0;
+let computerWinCount = 0;
+
 
 rock.addEventListener("click", function() {
   playRound(this.id, computerPlay())
@@ -60,6 +77,5 @@ paper.addEventListener("click", function() {
 scissors.addEventListener("click", function() {
   playRound(this.id, computerPlay())
 });
-
 
 // console.log(game());
